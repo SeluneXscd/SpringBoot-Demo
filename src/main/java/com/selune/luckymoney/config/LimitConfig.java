7a6935cf3c19905c24b5b29cec287e4ec5356ad3
SeluneXscd <1353977352@qq.com>
2019-05-25 16:33:10 +0800

    fix: add config 限制配置，minMoney maxMoney

---
+++ src/main/java/com/selune/luckymoney/config/LimitConfig.java
@@ -0,0 +1,46 @@
+package com.selune.luckymoney.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+import java.math.BigDecimal;
+
+/**
+ * @Author: Selune
+ * @Date: 2019/5/23 14:34
+ */
+
+@ConfigurationProperties(prefix = "limit")
+@Component
+public class LimitConfig {
+
+    private BigDecimal minMoney;
+
+    private BigDecimal maxMoney;
+
+    private String description;
+
+    public BigDecimal getMinMoney() {
+        return minMoney;
+    }
+
+    public void setMinMoney(BigDecimal minMoney) {
+        this.minMoney = minMoney;
+    }
+
+    public BigDecimal getMaxMoney() {
+        return maxMoney;
+    }
+
+    public void setMaxMoney(BigDecimal maxMoney) {
+        this.maxMoney = maxMoney;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
