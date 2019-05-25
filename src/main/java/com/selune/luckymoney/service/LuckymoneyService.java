3d0698cb7998dc31f9a9daa7dba4b6ce719939c7
SeluneXscd <1353977352@qq.com>
2019-05-25 16:36:43 +0800

    fix: add service

---
+++ src/main/java/com/selune/luckymoney/service/LuckymoneyService.java
@@ -0,0 +1,34 @@
+package com.selune.luckymoney.service;
+
+import com.selune.luckymoney.pojo.Luckymoney;
+import com.selune.luckymoney.repository.LuckymoneyRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.math.BigDecimal;
+
+/**
+ * @Author: Selune
+ * @Date: 5/25/19 3:57 PM
+ */
+
+@Service
+public class LuckymoneyService {
+
+    @Autowired
+    private LuckymoneyRepository repository;
+
+    @Transactional
+    public void createTwo() {
+        Luckymoney luckymoney1 = new Luckymoney();
+        luckymoney1.setProducer("张三");
+        luckymoney1.setMoney(new BigDecimal("520"));
+        repository.save(luckymoney1);
+
+        Luckymoney luckymoney2 = new Luckymoney();
+        luckymoney2.setProducer("张三");
+        luckymoney2.setMoney(new BigDecimal("1314"));
+        repository.save(luckymoney2);
+    }
+}
